20e820ae561da6c5fa0d1684172afb0457052d00
chjanusa <chjanusa@groupon.com>
2020-01-03 14:12:06 -0600

    Cleanup

--- randomizer/src/main/java/com/chrisjanusa/randomizer/location_search/updaters/LastManualLocationUpdater.kt
+++ randomizer/src/main/java/com/chrisjanusa/randomizer/location_search/updaters/LastManualLocationUpdater.kt
@@ -3,13 +3,8 @@ package com.chrisjanusa.randomizer.location_search.updaters
 import com.chrisjanusa.randomizer.base.interfaces.BaseUpdater
 import com.chrisjanusa.randomizer.base.models.RandomizerState
 
-class LastManualLocationUpdater(
-    private val locationStr: String
-) :
-    BaseUpdater {
+class LastManualLocationUpdater(private val locationStr: String) : BaseUpdater {
     override fun performUpdate(prevState: RandomizerState): RandomizerState {
-        return prevState.copy(
-            lastManualLocationText = locationStr
-        )
+        return prevState.copy(lastManualLocationText = locationStr)
     }
 }
