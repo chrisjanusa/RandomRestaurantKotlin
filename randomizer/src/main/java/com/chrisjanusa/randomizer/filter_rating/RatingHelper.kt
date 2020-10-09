2c57ea1e98e0227f03e7940574bd5730d7888ded
Chris Janusa <cmj160130@utdallas.edu>
2020-10-08 17:44:13 -0700

    Change X icon (#58)

--- randomizer/src/main/java/com/chrisjanusa/randomizer/filter_rating/RatingHelper.kt
+++ randomizer/src/main/java/com/chrisjanusa/randomizer/filter_rating/RatingHelper.kt
@@ -1,7 +1,16 @@
 package com.chrisjanusa.randomizer.filter_rating
 
+import kotlin.math.roundToInt
+
 const val defaultRating = 0f
 
-fun ratingToDisplayString(distance: Float) = "Min %.1f Stars".format(distance)
+fun ratingToDisplayString(distance: Float) : String {
+    val intDistance = distance.roundToInt()
+    return if (intDistance.toFloat() == distance) {
+        "Min $intDistance Stars"
+    } else {
+        "Min %.1f Stars".format(distance)
+    }
+}
 
 fun isDefault(rating: Float) = rating == defaultRating
