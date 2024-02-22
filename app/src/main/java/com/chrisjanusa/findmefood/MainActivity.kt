8dc4963818604cf408a504cf10dc2d6aeb21c428
chrisjanusa <chrisjanusa@gmail.com>
2024-02-22 15:25:31 -0800

    Add temporary error message

--- app/src/main/java/com/chrisjanusa/findmefood/MainActivity.kt
+++ app/src/main/java/com/chrisjanusa/findmefood/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.chrisjanusa.findmefood
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -39,11 +43,16 @@ class MainActivity : OverlayFragmentManager, AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = Navigation.findNavController(this, R.id.my_nav_host_fragment)
-        // TODO: Synthetics
-//        nav_view.setupWithNavController(navController)
-//        nav_view.setOnNavigationItemReselectedListener {
-            // Do nothing to ignore the reselection
-//        }
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.error_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog.show()
+//        val navController = Navigation.findNavController(this, R.id.my_nav_host_fragment)
+//        // TODO: Synthetics
+////        nav_view.setupWithNavController(navController)
+////        nav_view.setOnNavigationItemReselectedListener {
+//            // Do nothing to ignore the reselection
+////        }
     }
 }
